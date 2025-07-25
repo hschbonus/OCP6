5ecb551a5e4884e2487b0c1c24853240a08020f2
Hervé Schmidt <h.schmidt@hotmail.fr>
2025-07-25 18:54:01 +0200

    webp convert

--- routes/book.js
+++ routes/book.js
@@ -2,15 +2,16 @@ const express = require('express');
 const auth = require('../middleware/auth');
 const router = express.Router();
 const multer = require('../middleware/multer-config');
+const convertImageToWebp = require('../middleware/convert');
 const booksCtrl = require('../controllers/book');
 
-router.post('/', multer, booksCtrl.createBook);
+router.post('/', multer, convertImageToWebp, booksCtrl.createBook);
 router.get('/', booksCtrl.getAllBooks);
 router.get('/bestrating', booksCtrl.getTop3Books);
 router.get('/:id', booksCtrl.getOneBook);
 router.delete('/:id', auth, multer, booksCtrl.deleteBook);
 router.post('/:id/rating', auth, booksCtrl.addGrade);
-router.put('/:id', auth, multer, booksCtrl.updateBook);
+router.put('/:id', auth, multer, convertImageToWebp, booksCtrl.updateBook);
 
 
 module.exports = router;
