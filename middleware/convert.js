764663afae5d3a7bf741a9a3622799c772fc346e
Hervé Schmidt <h.schmidt@hotmail.fr>
2025-07-25 19:00:45 +0200

    convert edit

---
+++ middleware/convert.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const sharp = require('sharp');
+const path = require('path');
+
+const convertImageToWebp = async (req, res, next) => {
+  if (!req.file) return next();
+
+  try {
+    const webpPath = req.file.path.replace(/\.(jpg|jpeg|png)$/, '.webp');
+    await sharp(req.file.path).webp({ lossless: true }).toFile(webpPath);
+
+    fs.unlinkSync(req.file.path);
+    req.file.filename = path.basename(webpPath);
+    req.file.path = webpPath;
+
+    next();
+  } catch (error) {
+    console.error('Erreur convertImageToWebp:', error);
+    next(error);
+  }
+};
+
+module.exports = convertImageToWebp;
